83a03f7e6c2b89b544b9e633c99e8b284d8a6d7a
Paulo Batista <paulo.batista025@outlook.com>
2025-08-29 17:24:53 -0300

    desafio 01 Curso devsuperior

--- estudos/src/main/java/com/_paulo/estudos/EstudosApplication.java
+++ estudos/src/main/java/com/_paulo/estudos/EstudosApplication.java
@@ -1,7 +1,7 @@
 package com._paulo.estudos;
 
-import com._paulo.estudos.entities.Employee;
-import com._paulo.estudos.services.SalaryService;
+import com._paulo.estudos.entities.Order;
+import com._paulo.estudos.services.OrderService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,10 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EstudosApplication implements CommandLineRunner {
 
-	private SalaryService salaryService;
+	private OrderService orderService;
 
-	public EstudosApplication(SalaryService salaryService) {
-		this.salaryService = salaryService;
+
+	public EstudosApplication(OrderService orderService) {
+		this.orderService = orderService;
 	}
 
 	public static void main(String[] args) {
@@ -22,7 +23,17 @@ public class EstudosApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Employee employee = new Employee("Paulo", 4000.0);
-		System.out.println(salaryService.netSalary(employee));
+		// Exemplo 1
+		Order order1 = new Order(1034, 150.00, 20.0);
+		System.out.println("Pedido código: " + order1.getCode() + " Valor total: R$" + (String.format("%.2f", orderService.total(order1))));
+
+		// Exemplo 2
+		Order order2 = new Order(2282, 800.00, 10.0);
+		System.out.println("Pedido código: " + order2.getCode() + " Valor total: R$" + (String.format("%.2f", orderService.total(order2))));
+
+		// Exemplo 3
+		Order order3 = new Order(1309, 95.90, 0);
+		System.out.println("Pedido código: " + order3.getCode() + " Valor total: R$" + (String.format("%.2f", orderService.total(order3))));
 	}
+
 }
---
+++ estudos/src/main/java/com/_paulo/estudos/services/ShippingService.java
@@ -0,0 +1,21 @@
+package com._paulo.estudos.services;
+
+import com._paulo.estudos.entities.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShippingService {
+
+    public double shipment(Order order) {
+
+        if (order.getBasic() < 100.0) {
+            return 20.0;
+        }
+        else if (order.getBasic() >= 100.0 && order.getBasic() < 200.0) {
+            return 12.0;
+        }
+        else {
+            return 0.0;
+        }
+    }
+}
